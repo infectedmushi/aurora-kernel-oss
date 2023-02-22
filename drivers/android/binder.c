ef54c7e4c5b614294ead1cef68eb2351cb9effdc
dawfukfr <dawfukfr@gmail.com>
2023-02-22 18:44:56 +0100

    Revert "binder: use euid from cred instead of using task"
    This reverts commit 5d40061285b81a7e213dc9b37acc4a0545eedf32.

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -3796,7 +3796,7 @@ static void binder_transaction(struct binder_proc *proc,
 		t->from = thread;
 	else
 		t->from = NULL;
-	t->sender_euid = proc->cred->euid;
+	t->sender_euid = task_euid(proc->tsk);
 	t->to_proc = target_proc;
 	t->to_thread = target_thread;
 	t->code = tr->code;
@@ -5612,7 +5612,6 @@ static void binder_free_proc(struct binder_proc *proc)
 	}
 	binder_alloc_deferred_release(&proc->alloc);
 	put_task_struct(proc->tsk);
-	put_cred(proc->cred);
 	binder_stats_deleted(BINDER_STAT_PROC);
 	kfree(proc);
 }
@@ -6329,7 +6328,6 @@ static int binder_open(struct inode *nodp, struct file *filp)
 #ifdef OPLUS_FEATURE_SCHED_ASSIST
 	proc->proc_type = is_binder_proc_sf(proc) ? 1 : 0;
 #endif /* OPLUS_FEATURE_SCHED_ASSIST */
-	proc->cred = get_cred(filp->f_cred);
 	INIT_LIST_HEAD(&proc->todo);
 	init_waitqueue_head(&proc->freeze_wait);
 	if (binder_supported_policy(current->policy)) {
